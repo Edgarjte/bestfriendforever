075989b71e0e537704e54b74fab941156aa33e2b
Edgarjte <jetteedgar.pro@gmail.com>
2026-02-17 00:00:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("fireworks");
 const ctx = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -52,8 +53,14 @@ yesButton.addEventListener("click", () => {
 
   document.querySelector(".card").innerHTML = `
     <h1 style="color:#7b1e3a">
-      OUIIIIIII<br><br>
-      Coeur sur toi ma Leo ♥
+      OUIIIII !<br><br>
+      Coeur sur toi ma Léo ♥
     </h1>
+
+    <img 
+      src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWhyNW0xNmMwbWN4Z2k5dTlxcnE2OXMzcnZnNWJ4OHN2OWlwMDk4biZlcD12MV9naWZzX3NlYXJjaCZjdD1n/MenglW7Bcu89JWTjeD/giphy.gif"
+      alt="gif de fin"
+      class="final-gif"
+    >
   `;
 });
